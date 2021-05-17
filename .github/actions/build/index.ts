b4476d5c6eaa806ada81a3d26501bfd0380c73e0
networkException <git@nwex.de>
2021-05-17 17:51:28 +0200

    CI: Dump github context

--- .github/actions/build/index.ts
+++ .github/actions/build/index.ts
@@ -1,9 +1,3 @@
 import * as github from '@actions/github';
-import * as artifact from '@actions/artifact';
-import { ArtifactClient } from '@actions/artifact';
 
-const artifactClient: ArtifactClient = artifact.create();
-
-artifactClient.uploadArtifact('test', [ '/home/runner/work/workflow-testing/workflow-testing/.github/actions/build/index.ts' ], '.');
-
-console.log(github.context.runNumber);
+console.log(github.context);
