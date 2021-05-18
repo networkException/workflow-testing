2553d3b95c470af5b215ae0276796d98ddfa2daf
networkException <git@nwex.de>
2021-05-18 10:07:49 +0200

    CI: Await order

--- .github/actions/build/index.ts
+++ .github/actions/build/index.ts
@@ -1,9 +1,9 @@
-import * as github from '@actions/github';
-import * as core from '@actions/core';
 import * as arifactHost from '@actions/artifact';
 import * as shell from '@actions/exec';
 
 const artifact = arifactHost.create();
 
-artifact.downloadArtifact('test');
-shell.exec('ls -lah');
+(async () => {
+    await artifact.downloadArtifact('test');
+    await shell.exec('ls -lah');
+})();
