550f3db371eda4de0d84c249d2e3967d9851ad4f
networkException <git@nwex.de>
2021-05-22 15:39:55 +0200

    CI: Fix octokit request

--- .github/actions/build/index.ts
+++ .github/actions/build/index.ts
@@ -3,7 +3,10 @@ import * as github from '@actions/github';
 
 const octokit = github.getOctokit(core.getInput('token', { required: true }));
 
-octokit.request(`GET /orgs/networkException/packages/container/workflow-testing`, { 
+octokit.request(`GET /orgs/{org}/packages/{package_type}/{package_name}`, {
+    org: 'networkException',
+    package_type: 'container',
+    package_name: 'workflow-testing',
     headers: {
         accept: 'application/vnd.github.v3+json'
     }
