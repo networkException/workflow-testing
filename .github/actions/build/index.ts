8dc89a7ba3c64e412c366181c06bc0ce17083952
networkException <git@nwex.de>
2021-05-22 14:55:30 +0200

    CI: Test with container pushed by own repo

--- .github/actions/build/index.ts
+++ .github/actions/build/index.ts
@@ -3,7 +3,7 @@ import * as github from '@actions/github';
 
 const octokit = github.getOctokit(core.getInput('token', { required: true }));
 
-octokit.request(`GET /orgs/${github.context.repo.owner}/packages/container/ungoogled-chromium-archlinux`, { 
+octokit.request(`GET /orgs/networkException/packages/container/workflow-testing`, { 
     headers: {
         accept: 'application/vnd.github.v3+json'
     }
