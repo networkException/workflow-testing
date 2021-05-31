0b450426c715f76e0004eae92bffd6c1e0afa65e
networkException <git@nwex.de>
2021-05-31 16:23:29 +0200

    CI: Test removing android directory

--- .github/actions/build/index.ts
+++ .github/actions/build/index.ts
@@ -1,12 +1,5 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import * as exec from '@actions/exec';
 
-const octokit = github.getOctokit(core.getInput('token', { required: true }));
-
-octokit.request(`GET /user/packages/{package_type}/{package_name}`, {
-    package_type: 'container',
-    package_name: 'workflow-testing',
-    headers: {
-        accept: 'application/vnd.github.v3+json'
-    }
-}).then(console.log);
+(async () => {
+    await exec.exec('sudo rm -rf /usr/local/lib/android');
+})();
