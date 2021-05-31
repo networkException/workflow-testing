6514d1f74ddfefee4f02827be902abfc7c85bffc
networkException <git@nwex.de>
2021-05-31 16:40:54 +0200

    CI: Output the amount of space freed by removing dependencies

--- .github/actions/build/index.ts
+++ .github/actions/build/index.ts
@@ -1,7 +1,13 @@
 import * as exec from '@actions/exec';
 
 (async () => {
-    await exec.exec('id');
-    await exec.exec('sudo id');
-    await exec.exec('rm -rf /usr/local/lib/android');
+    await exec.exec('wget https://github.com/muesli/duf/releases/download/v0.6.2/duf_0.6.2_linux_amd64.deb');
+    await exec.exec('sudo apt install ./duf_0.6.2_linux_amd64.deb');
+    await exec.exec('duf');
+
+    await exec.exec('sudo rm -rf /usr/share/dotnet');
+    await exec.exec('sudo rm -rf /usr/local/lib/android');
+    await exec.exec('sudo rm -rf /opt/ghc');
+
+    await exec.exec('duf');
 })();
